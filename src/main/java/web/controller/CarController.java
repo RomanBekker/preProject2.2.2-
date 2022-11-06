cfe1fde0881bb8e826063e1d69edb446ef819e93
Roman Bekker <bekker.roman@list.ru>
2022-11-06 19:58:07 +0300

    commit 3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
-import static web.service.CarService.createCars;
 
 @Controller
 public class CarController {
 
+    CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(defaultValue = "5", value = "count", required = false) int count) {
-        model.addAttribute("cars", createCars(count));
+        model.addAttribute("cars", carService.createCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,21 +1,25 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Service
 public class CarService {
 
-    public static List<Car> createCars(int count) {
+    Car car1 = new Car("Audi", "blue", 120);
+    Car car2 = new Car("Bentley", "red", 140);
+    Car car3 = new Car("BMW", "yellow", 150);
+    Car car4 = new Car("SKODA", "white", 170);
+    Car car5 = new Car("LADA", "black", 200);
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Audi", "blue", 120));
-        cars.add(new Car("Bentley", "red", 140));
-        cars.add(new Car("BMW", "yellow", 150));
-        cars.add(new Car("SKODA", "white", 170));
-        cars.add(new Car("LADA", "black", 200));
+    List<Car> cars = Stream.of(car1, car2, car3, car4, car5).collect(Collectors.toCollection(ArrayList::new));
 
+    public List<Car> createCars(int count) {
         return count <= 5 ? cars.subList(0, count) : cars;
     }
 }
